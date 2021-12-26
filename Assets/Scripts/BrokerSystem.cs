e939b78f75f8067f58cc6ab5518832264827678d
Ramon Melo <ramonmelo@ramonmelo.com>
2021-12-26 18:25:36 -0300

    Added: Broker System

---
+++ Assets/Scripts/BrokerSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public static class BrokerSystem {
+
+	
+	public static event Action OnStartGame;
+	
+	
+	public static void StartGame() {
+		Debug.Log($"Running: {nameof(StartGame)}");
+		OnStartGame?.Invoke();
+	}
+	
+}
