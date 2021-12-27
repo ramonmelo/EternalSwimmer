67ceb38d630e2468a0d635f05817fd9c67b7e397
Ramon Melo <ramonmelo@ramonmelo.com>
2021-12-27 20:20:50 -0300

    Added: Broker System

--- Assets/Scripts/BrokerSystem.cs
+++ Assets/Scripts/BrokerSystem.cs
@@ -6,10 +6,17 @@ public static class BrokerSystem {
 	
 	public static event Action OnStartGame;
 	
+	public static event Action OnEndGame;
+	
 	
 	public static void StartGame() {
 		Debug.Log($"Running: {nameof(StartGame)}");
 		OnStartGame?.Invoke();
+	}
+	
+	public static void EndGame() {
+		Debug.Log($"Running: {nameof(EndGame)}");
+		OnEndGame?.Invoke();
 	}
 	
 }
