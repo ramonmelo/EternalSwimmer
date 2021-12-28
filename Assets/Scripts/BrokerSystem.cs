be883ef7c960ebb7126d32c0634cc06393bd99c2
Ramon Melo <ramonmelo@ramonmelo.com>
2021-12-28 11:34:51 -0300

    Added: Game Constants

--- Assets/Scripts/BrokerSystem.cs
+++ Assets/Scripts/BrokerSystem.cs
@@ -3,13 +3,7 @@ using UnityEngine;
 
 public static class BrokerSystem {
 
-	
 	public static event Action OnStartGame;
-	
-	
-	public static void StartGame() {
-		Debug.Log($"Running: {nameof(StartGame)}");
-		OnStartGame?.Invoke();
-	}
-	
+
+  public static void StartGame() => OnStartGame?.Invoke();
 }
---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,4 @@
+public static class GameConstants {
+  public const string GAME_SCENE = "GameScene";
+  public const string MENU_SCENE = "MenuScene";
+}
